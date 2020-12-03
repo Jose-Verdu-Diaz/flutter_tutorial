51f3b79eff6f4d95f4d5d298e45b7c6101174b20
Jose-Verdu-Diaz <pepv@hotmail.es>
2020-12-03 08:53:30 +0100

    Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,55 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ['Q1', 'Q2', 'Q3', 'Q4'];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Hola Mundo!'),
         ),
-        body: Text('Lorem ipsum'),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: () => print('Answer 2 Chosen'),
+            ),
+            RaisedButton(
+              child: Text('Answer3'),
+              onPressed: () {
+                print('Answer 3 Chosen');
+              },
+            )
+          ],
+        ),
       ),
     );
   }
